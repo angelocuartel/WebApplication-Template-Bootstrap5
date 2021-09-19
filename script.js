73da86c9ca6b0be8d6ad615975e807b60ebd63b5
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-09-19 08:47:20 +0800

    add smallscreen navbar script

--- script.js
+++ script.js
@@ -4,21 +4,48 @@ const tempNavbar = document.querySelector(".temp-sidebar");
 const navBar = document.querySelector(".sidebar");
 const header = document.querySelector(".sidebar-header")
 
+let isScreenSizeSmall = false;
+
+
+btn.addEventListener("click",resizeTempNavbar);
+window.onresize = toggleSmallScreenNavbar
+window.onload = toggleSmallScreenNavbar
 
 
 
-
-btn.onclick = function(){
-
-
-
-    if(tempNavbar.style.width == "250px"){
-        navBar.style.width = tempNavbar.style.width = "35px";
-        header.innerHTML ='';
-    }
-    else{
-        navBar.style.width = tempNavbar.style.width = "250px";
+function resizeTempNavbar(){
+    if(navBar.clientWidth == 250 )toggleNavbar("","35px");
+    else if(isScreenSizeSmall){
+        navBar.style.width = "250px";
         header.innerHTML = "SYSTEM";
+        tempNavbar.style.width = "35px";
     }
+    else if(tempNavbar.clientWidth != 250 )
+    toggleNavbar('SYSTEM',"250px");
 
+}
+
+function toggleSmallScreenNavbar(){
+
+    if(window.innerWidth <= 600){
+
+        toggleNavbar("","35px");
+        isScreenSizeSmall = true;
+  
+        /*
+         
+          tempNavbar.style.width
+          alert("small screen");*/
+  
+  
+      }
+      else{
+          isScreenSizeSmall = false;
+      }
+  
+}
+
+function toggleNavbar(textHeader,navbarWidth){
+    navBar.style.width = tempNavbar.style.width = navbarWidth;
+    header.innerHTML = textHeader;
 }
